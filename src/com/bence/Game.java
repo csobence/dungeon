5783239e1d42f5f85b50760c7a08540ee7edc05c
csobence <97903794+csobence@users.noreply.github.com>
2022-01-18 12:48:16 +0100

    Update Game.java

--- src/com/bence/Game.java
+++ src/com/bence/Game.java
@@ -68,32 +68,32 @@ public class Game {
     public void abilitiList() {
         System.out.println("Strike: deals psychical damage to the enemy");
         System.out.println("Magic: deals magical damage to the enemy and costs 4 mana");
-        if (classes.isWarrior()) {
+        if (classes.role.equals(Role.WARRIOR)) {
             System.out.println("Bloodbath: your next strike restore your health by the amount of damage you have dealt");
-        } else if (classes.isMage()) {
+        } else if (classes.role.equals(Role.MAGE)) {
             System.out.println("Fireball: deals a huge magical damage and costs 10 mana");
-        } else if (classes.isPriest()) {
+        } else if (classes.role.equals(Role.PRIEST)) {
             System.out.println("Heal: restore your own health");
-        } else if (classes.isRogue()) {
+        } else if (classes.role.equals(Role.ROGUE)) {
             System.out.println("Stealth: you become invisible and you can use backstab or flee");
             System.out.println("Backstab: deals a huge pure damage to the enemy, it breaks stealth");
             System.out.println("Flee: you can flee without taking damage");
-        } else if (classes.isSamurai()) {
+        } else if (classes.role.equals(Role.SAMURAI)) {
             System.out.println("Iaido: you sheath your weapon for one turn, and after you next time use iaido, you deals " +
                     "*4 psychical damage");
-        } else if (classes.isBerserker()) {
+        } else if (classes.role.equals(Role.BERSERKER)) {
             System.out.println("Rampage: take off half your health and increases your psychical damage that amount");
             System.out.println("Passive: you get more physical damage the lower your health are");
-        } else if (classes.isWarlock()) {
+        } else if (classes.role.equals(Role.WARLOCK)) {
             System.out.println("Shadowbolt: sacrifice 8 health for a huge magical damage");
             System.out.println("Passive: your magic skill restores health");
-        } else if (classes.isPaladin()) {
+        } else if (classes.role.equals(Role.PALADIN)) {
             System.out.println("Reflect: you will reflect the 100% of your opponent's next damage for 6 mana");
             System.out.println("Passive: you have 30% chance to block the incoming physical attacks");
-        } else if (classes.isGunslinger()) {
+        } else if (classes.role.equals(Role.GUNSLINGER)) {
             System.out.println("Barrage: deals psychical damage if you used strike before or magical damage if you used magic before");
             System.out.println("Passive: your barrage has a chance to shot again");
-        } else if (classes.isGambler()) {
+        } else if (classes.role.equals(Role.GAMBLER)) {
             System.out.println("Roll: You roll a D6.");
             System.out.println("If you roll 1: Auto flee from the battle");
             System.out.println("If you roll 2: take off half of your health");
@@ -102,9 +102,9 @@ public class Game {
             System.out.println("If you roll 5: deals magical damage * 3 damage");
             System.out.println("If you roll 6: deals 45 pure damage");
             System.out.println("Passive: you only get good items from chests");
-        } else if (classes.isNecromancer()) {
+        } else if (classes.role.equals(Role.NECROMANCER)) {
             System.out.println("Claws: command your undead minions to attack, deals psychical damage");
-        } else if (classes.isShapeshifter()) {
+        } else if (classes.role.equals(Role.SHAPESHIFTER)) {
             System.out.println("Transform: you can transform into a defeated monster");
         }
         System.out.println("Flee: you can flee from the battle");

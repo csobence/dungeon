15841381645177144d395409100a9f0f1d463686
csobence <97903794+csobence@users.noreply.github.com>
2022-01-28 12:00:09 +0100

    Update Main.java

--- src/com/bence/Main.java
+++ src/com/bence/Main.java
@@ -5,17 +5,21 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
+        Scanner scanner = new Scanner(System.in);
         Classes classes = new Classes();
         Arts arts = new Arts();
-        Game game = new Game(classes, arts);
         Monsters monsters = new Monsters();
+        Items items = new Items(classes, scanner);
+        Game game = new Game(classes, arts);
+        Combat combat = new Combat(game, classes, arts, items, monsters, scanner);
+        Weapon weapon = new Weapon(classes, arts, scanner, items);
+        Armor armor = new Armor(classes, arts, scanner, items);
+        Chest chest = new Chest(classes, arts, scanner, items, weapon, armor, monsters);
+        Traproom traproom = new Traproom(game, classes, combat, monsters);
+        Campfire campfire = new Campfire(combat, arts, classes);
 
-        Scanner scanner = new Scanner(System.in);
-        UserInterface userInterface = new UserInterface(game, classes, monsters, scanner, arts);
+        UserInterface userInterface = new UserInterface(game, classes, monsters, scanner, arts, combat, items, chest, traproom, campfire, weapon, armor);
 
         userInterface.startGame();
-
-
     }
 }
-
